4d5556801d6ee1bb9de4c782c45ed11346b9b645
Hasitha Samarasekara <hasitha.dks@gmail.com>
2020-08-20 00:21:52 +0530

    Add Form and controller to Hansaka

---
+++ TimeTableManagementSystemApp/TimeTableManagementSystemApp/IT18049114/UserControllers/UserControl1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TimeTableManagementSystemApp.IT18049114.UserControllers
+{
+    public partial class UserControl1 : UserControl
+    {
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+    }
+}
