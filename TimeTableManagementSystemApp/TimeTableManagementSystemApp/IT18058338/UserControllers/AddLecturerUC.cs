7f5d838961ff6834075ebde3eba45368bd75850b
Gihan76 <gihantharaka76@gmail.com>
2020-08-20 23:09:15 +0530

    Commit Lecturers,Subjects

---
+++ TimeTableManagementSystemApp/TimeTableManagementSystemApp/IT18058338/UserControllers/AddLecturerUC.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TimeTableManagementSystemApp.IT18058338.UserControllers
+{
+    public partial class AddLecturerUC : UserControl
+    {
+        public AddLecturerUC()
+        {
+            InitializeComponent();
+        }
+
+        private void labeldepartment_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxdepartment_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
